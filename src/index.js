31910276ec2edee9e14beae6e58b440076e74af2
Rogerio Batista <56281188+rogeriobatista@users.noreply.github.com>
2025-03-24 19:06:17 -0300

    Avoid redirect loop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 export default {
   async fetch(request, env, ctx) {
-    let url = new URL(request.url);
-    let fileName = url.pathname.replace("/downloads/", "");
-    let newUrl = "https://microcad.blob.core.windows.net/downloads/" + fileName;
-    return Response.redirect(newUrl, 301);
+    const url = new URL(request.url)
+  
+    if (url.hostname === 'microcad.blob.core.windows.net') {
+      return fetch(request)
+    }
+    
+    const dynamicPath = url.pathname.split('/').slice(-1)[0]
+    
+    const newUrl = `https://microcad.blob.core.windows.net/downloads/${dynamicPath}`
+
+    return Response.redirect(newUrl, 301)
   },
 };
